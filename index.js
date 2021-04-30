2ffbdae90683ddafae759bcb2869adc12fe4a394
RocioAl <koroinice@gmail.com>
2021-04-30 15:45:39 -0500

    cors npm

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const { req, res } = require('express');
-bodyParser = require('body-parser')
-cookieParser = require('cookie-parser')
-cors = require('cors');
 
-
-// For each request, provide wildcard Access-Control-* headers via OPTIONS call
-app.use(cors());
-
-// For each request, parse request body into a JavaScript object where header Content-Type is application/json
-app.use(bodyParser.json());
-
-// For each request, parse cookies
-app.use(cookieParse());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
-// app.use(cors())
-// app.get('/con-cors', cors(), (req, res, next) => {
-//     res.json({ msg: 'con cors 🔝 🎉' })
-// })
+app.use(cors())
+app.get('/con-cors', cors(), (req, res, next) => {
+    res.json({ msg: 'con cors 🔝 🎉' })
+})
 app.get("/api", (req, res) => {
     res.json({
         mensaje: "Nodejs and JWT"
