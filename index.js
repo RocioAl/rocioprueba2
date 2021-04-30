117ab80120ea709b18bbe7b735ae262befa7a0be
RocioAl <koroinice@gmail.com>
2021-04-30 13:27:14 -0500

    assfas

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const app = express();
-// const cors = require('cors');
+const cors = require('cors');
 const { req, res } = require('express');
 
 const port = process.env.PORT || 5000;
